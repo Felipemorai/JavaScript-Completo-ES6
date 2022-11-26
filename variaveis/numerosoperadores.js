0ece222148e284cb8b995813ecebcba9ab786139
Luiz Felipe <felipemorai188@gmail.com>
2022-11-26 12:00:05 -0300

    NaN

---
+++ variaveis/numerosoperadores.js
@@ -0,0 +1,29 @@
+var idade = 20;
+var gols = 1000;
+var pi = 3.14;
+var exp = 2e10;
+
+console.log(exp);
+
+// Operadores Aritméticos
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50;
+var multiplicacao = 100 * 2; // 200;
+var dividao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+var sum = '100' + 50; // 10050
+var testeNaN = 'Comprei 10' / 2; // NaN (Not a Number)
+
+console.log(isNaN(sum));
+console.log(isNaN(testeNaN));
+
+// NaN = Not a Number
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade; // 80kg
+var pesoPorDois = peso / 2; // NaN a Number
+
+console.log(peso);
+console.log(pesoPorDois);
