4aa9f6671e022f1f17fc9be377ff8fd60af46b2b
Luiz Felipe <felipemorai188@gmail.com>
2022-11-28 12:13:15 -0300

    switch

--- variaveis/boolean.js
+++ variaveis/boolean.js
@@ -73,3 +73,84 @@ if(!!'')  // false
 10 <= 10 // true
 10 >= 11 // false
 */
+
+// Operadores de comparação
+/* O == faz uma comparação não tão estrita e o === faz uma comparação estrita, ou seja, o tipo de dado deve ser o mesmo quando usamos === */
+10 == '10'; // true
+10 == 10; // true
+10 === '10'; // false
+10 === 10; // true
+10 != 15 // true
+10 != '10' // false
+10 !== '10' // true
+
+var x = 10;
+var y = 'Gatos';
+
+console.log(x == 10);
+console.log(y !== 'Gato');
+
+// Operadores Lógicos &&
+// && Compara se uma expressão e a outra é verdadeira
+
+true && true; // true
+true && false; // false
+false && true; // false
+'Gato' && 'Cão'; // 'Cão'
+(5 - 5) && (5 + 5); // 0
+'Gato' && false; // false
+(5 >= 5) && (3 < 6); // true
+
+if((5 - 5) && (5 + 5)) {
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
+
+var condicional = (5 - 10) && (5 + 5);
+
+if(condicional) {
+    console.log('Verdadeiro', condicional);
+} else {
+    console.log('Falso');
+}
+
+// Operadores Lógicos || 
+// || Compara se uma expressão ou outra é verdadeira
+true || true; // true
+true || false; // true
+false || true; // true
+'Gato' || 'Cão'; // 'Gato'
+(5 - 5) || (5 + 5); // 10
+'Gato' || false; // Gato
+(5 >= 5) || (3 < 6); // true
+
+var condicional2 = (5 - 5) || (5 + 5) || (10 - 2);
+console.log(condicional2);
+
+var condicional3 = (5 - 5) || (5 + 5) && (10 - 2);
+console.log(condicional3);
+
+// Switch
+/* Com o switch você pode verificar se uma variável é igual à diferentes valores utilizando o case. Caso ela seja igual, você pode fazer alguma coisa e utilizar a palavra chave break; para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira. */
+var corFavorita = 'Azul';
+
+switch (corFavorita) {
+    case 'Azul':
+        console.log('Olhe para o céu.');
+        break;
+    case 'Vermelho':
+        console.log('Olhe para as rosas.');
+        break;
+    case 'Verde':
+        console.log('Olhe para a floresta.');
+        break;
+    case 'Amarelo':
+        console.log('Olhe para o sol.');
+        break;
+    case 'Branco':
+        console.log('Olhe para as nuvens.');
+        break;
+    default:
+        console.log('Feche os olhos');
+}
