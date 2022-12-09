306e2dc2cdd2fd5494e2e6d39b6e4b60d6b2fbbe
Luiz Felipe <felipemorai188@gmail.com>
2022-12-09 12:27:21 -0300

    this

--- variaveis/objetos.js
+++ variaveis/objetos.js
@@ -48,3 +48,52 @@ Math.random(); // número aleatório
 
 var pi = Math.PI;
 console.log(pi); // 3.14
+
+// Criar um Objeto
+// Um objeto é criado utilizando as chaves {}
+var carro = {};
+var person = {};
+
+console.log(typeof carro); // 'object'
+
+// Dot Notation Get and Set
+// Acesse propriedades de um objeto utilizando o ponto .
+// Substitua o valor de uma propriedade utilizando . e o = após o nome da mesma
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: "#84e",
+}
+
+menu.color = '#FFF';
+
+menu.esconder = function() {
+    console.log('Esconder');
+}
+
+var bg = menu.backgroundColor; // "#84e"
+var cl = menu.color; // "#FFF"
+
+// Adicionar Propriedades e Métodos
+// Basta adicionar um novo nome e definir o valor.
+
+var menuAdd = {
+    width: 800,
+}
+
+menuAdd.height = 50;
+menuAdd.position = 'fixed';
+
+// Palavra-chave this
+// this irá fazer uma referência ao próprio objeto.
+var height = 120;
+var menuThis = {
+    width: 800,
+    height: 50,
+    halfHeight() {
+        return this.height / 2;
+    }
+}
+
+menuThis.halfHeight(); // 25
+// sem o this pegaria o height de fora do objeto, e retornaria 60
