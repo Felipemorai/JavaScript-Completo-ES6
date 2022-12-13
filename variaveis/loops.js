3b5d8565f4426a23e8c497201dd6fd5657bcb05b
Luiz Felipe <felipemorai188@gmail.com>
2022-12-13 12:30:09 -0300

    loops

---
+++ variaveis/loops.js
@@ -0,0 +1,17 @@
+// For Loop
+// Fazem algo repetidamente até que uma condição seja atingida.
+for (var numero = 0; numero < 10; numero++) {
+    console.log(numero);
+}
+// Retorna de 0 a 9 no console
+// o for loop possui 3 partes,
+// início, condição e incremento
+
+// While Loop
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+// Retorna de 0 a 9 no console
+// O for loop é o mais comum
