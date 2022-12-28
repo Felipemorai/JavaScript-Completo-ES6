2e2c1731d605e97e336e5941240614e949e7fba7
Luiz Felipe <felipemorai188@gmail.com>
2022-12-28 16:53:56 -0300

    selecao

--- dom/animais-fantasticos/script.js
+++ dom/animais-fantasticos/script.js
@@ -10,11 +10,32 @@ const naoExiste = document.getElementById('teste');
 // Classe e Tag
 // getElementsByClassName e getElementsByTagName selecionam e retornam uma lista de elementos do DOM. A lista retornada está ao vivo, significa que se elementos forem adicionados, ela será automaticamente atualizada.
 // Seleciona pela classe, retorna uma HTMLCollection
-const gridSection = document.getElementsByClassName('.grid-section');
-const contato = document.getElementsByClassName('grid-section contato');
+const gridSectionId = document.getElementsByClassName('grid-section');
+const contatos = document.getElementsByClassName('grid-section contato');
 
 // Selecionar todas as UL's, retorna uma HTMLCollection
 const ul = document.getElementsByTagName('ul');
 
 // Retorna o primeiro elemento
-console.log(gridSection[0]);
+console.log(gridSectionId[0]);
+
+// Seletor Geral Único
+// querySelector retorna o primeiro elemento que combinar com o seu seletor CSS.
+const animais = document.querySelector('.animais');
+const contato = document.querySelector('#contato');
+const ultimoItem = document.querySelector('.animais-lista li:last-child');
+const linkCSS = document.querySelector('[href^="https://"]');
+const primeiroUL = document.querySelector('ul');
+
+// Busca dentro da UL apenas
+const navItem = primeiroUL.querySelector('li');
+
+// Seletor Geral Lista
+// querySelectorAll  retorna todos os elementos compatíveis com o seletor CSS em uma NodeList
+const gridSection = document.querySelectorAll('.grid-section')
+const listas = document.querySelectorAll('ul');
+const titulos = document.querySelectorAll('.titulo');
+const fotosAnimais = document.querySelectorAll('.animais-lista img');
+
+// Retorna o segundo elemento
+console.log(gridSection[1]);
