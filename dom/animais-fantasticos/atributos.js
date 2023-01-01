cc8a2b7306103e8855507c3e57faf3b1271bdaf3
Luiz Felipe <felipemorai188@gmail.com>
2023-01-01 14:13:57 -0300

    atributos

--- dom/animais-fantasticos/atributos.js
+++ dom/animais-fantasticos/atributos.js
@@ -24,3 +24,38 @@ const animais = document.querySelector('.animais');
 animais.attributes; // retorna todos os atributos
 animais.attributes[0]; // retorna o primeiro atributo
 
+// getAttribute e setAttribute
+// Métodos que retornam ou definem de acordo com o atributo selecionado
+const img = document.querySelector('img');
+
+img.getAttribute('src'); // valor do src
+img.setAttribute('alt', 'Texto Alternativo'); // muda o alt
+img.hasAttribute('id'); // true / false
+img.removeAttribute('alt'); // remove o alt
+
+img.hasAttributes(); // true / false se tem algum atributo
+// É mutio comum métodos de get e set;
+
+const srcImg = img.getAttribute('src');
+
+console.log(srcImg);
+
+const alt = img.setAttribute('alt', 'Raposa');
+
+console.log(img.getAttribute('alt'));
+
+const hasAlt = img.hasAttribute('alt');
+
+console.log(hasAlt);
+
+const hasTitle = img.hasAttribute('title');
+
+console.log(hasTitle);
+
+// Read Only vs Writable
+// Existem propriedades que não permitem a mudança de seus valores, essas são considerados Read Only, ou seja, apenas leitura.
+animais.className; // string com o nome das classes
+animais.className = 'azul'; // substitui completamente a string
+animais.className += 'vermelho'; // adiciona vermelho á string
+animais.attributes = 'class="ativo"'; // não funciona, read-only
+// Lembre-se que podemos modificar o valor de uma propriedade objeto.propriedade = ''
