d8b0dc4a458b2909047758bbacf0532a4d06147e
Luiz Felipe <felipemorai188@gmail.com>
2023-01-13 17:05:50 -0300

    accordian

---
+++ dom/animais-fantasticos/accordion.js
@@ -0,0 +1,10 @@
+const accordionList =  document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+    this.classList.add('ativo');
+    this.nextElementSibling.classList.add('ativo');
+};
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+});
