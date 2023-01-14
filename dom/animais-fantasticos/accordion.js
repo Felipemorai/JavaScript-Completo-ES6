18d7d47e391693d697e23b3700b3ee48162bda89
Luiz Felipe <felipemorai188@gmail.com>
2023-01-14 10:57:46 -0300

    toggle

--- dom/animais-fantasticos/accordion.js
+++ dom/animais-fantasticos/accordion.js
@@ -1,10 +1,15 @@
 const accordionList =  document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo';
+if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-function activeAccordion() {
-    this.classList.add('ativo');
-    this.nextElementSibling.classList.add('ativo');
-};
+    function activeAccordion() {
+        this.classList.toggle(activeClass);
+        this.nextElementSibling.classList.toggle(activeClass);
+    };
 
-accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion);
-});
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+    });
+}
