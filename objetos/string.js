dd1cd349adff7134334260b51de083527e51c384
Luiz Felipe <felipemorai188@gmail.com>
2023-02-03 13:55:31 -0300

    includes

--- objetos/string.js
+++ objetos/string.js
@@ -29,3 +29,32 @@ const frase2 = 'A melhor linguagem é ';
 const linguagem2 = 'JavaScript';
 
 const fraseCompleta = frase2.concat(linguagem2, '!!');
+
+// str.includes(search, position)
+const fruta = 'Banana';
+const listaFrutas = 'Melancia, Banana, Laranja';
+
+listaFrutas.includes(fruta); // true
+fruta.includes(listaFrutas); // false
+
+// str.endsWith(search) e str.startsWith(search)
+// Procura pela string exata dentro de outra string. A procura é case sensitive.
+fruta2 = 'Banana';
+
+fruta2.endsWith('nana'); // true
+fruta2.startsWith('Ba'); // tru
+fruta2.startsWith('na'); // false
+
+// str.slice(start, end)
+// Corta a string de acordo com os valores de start e end.
+const transacao1 =  'Depósito de cliente';
+const transacao2 = 'Depósito de fornecedor';
+const transacao3 = 'Taxa de camisas';
+
+transacao1.slice(0, 3); // Dep
+transacao2.slice(0, 3); // Dep
+transacao3.slice(0, 3); // Tax
+
+transacao1.slice(12); // cliente
+transacao2.slice(-4); // ente
+transacao3.slice(3, 6); // ósi
